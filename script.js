74bc80a77536ad064c3384b9bfda22c3af7573c3
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-07-25 22:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const byteSize = (str) => {
   // write your code here
-	// let len=0;
-	var arr=str.split('');
-	return arr.length;
+	const encoder = new TextEncoder();
+  const encodedString = encoder.encode(str);
+  return encodedString.length;
 };
 
 // Do not change the code below
-const str = prompt("Enter some string.")
+const str = prompt("Enter some string."); 
 alert(byteSize(str));
